40a2e484f68d272d67c57ef729efa270e120758c
ÇAĞATAY KARASU <61315022+cgtykarasu@users.noreply.github.com>
2024-02-22 00:32:35 +0300

    braking mechanics
    Braking with the space key was added
    Additionally, the speed value was adapted to this mode by updating it to ForceMode.Acceleration.

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -6,19 +6,28 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     // movement speed
-    public float moveSpeed = 30f;
+    public float moveSpeed = 1250f;
+
     // turn speed
     public float turnSpeed = 100f;
+
     // drift factor
     public float driftFactor = .95f;
-    
+
+    //breaking status
+    public bool isBraking = false;
+
+    //breakingforce
+    public float brakeForce = 0.985f;
+
+
     private Rigidbody rb;
 
     void Start()
     {
         // rigidbody component
         rb = GetComponent<Rigidbody>();
-        
+
         // center of mass
         rb.centerOfMass = new Vector3(0, -0.5f, 0);
     }
@@ -31,11 +40,11 @@ public class CarController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
 
         // create movement vector (forward direction)
-        Vector3 moveDirection = transform.forward * moveVertical * moveSpeed;
-        
+        Vector3 moveDirection = transform.forward * (moveVertical * moveSpeed);
+
         // apply force to the car
-        rb.AddForce(moveDirection * Time.deltaTime, ForceMode.VelocityChange);
-        
+        rb.AddForce(moveDirection * Time.deltaTime, ForceMode.Acceleration);
+
         // if car speed is greater than 2
         if (rb.velocity.magnitude > 2)
         {
@@ -46,7 +55,27 @@ public class CarController : MonoBehaviour
             // update rotation
             rb.MoveRotation(rb.rotation * turnRotation);
         }
-        
+
         // Debug.Log(rb.velocity.magnitude);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            isBraking = true;
+            rb.velocity *= brakeForce;
+        }
+        else
+        {
+            isBraking = false;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Platform"))
+        {
+            Debug.Log("DANGEEEERRRRR");
+            rb.useGravity = true;
+            //rb.velocity=Vector3.down;
+        }
     }
 }
