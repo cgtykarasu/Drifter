be0686669cdf6508f4b058dc9a651f6000f96d9e
ÇAĞATAY KARASU <61315022+cgtykarasu@users.noreply.github.com>
2024-03-05 03:19:23 +0300

    testing

--- Assets/TestCarController.cs
+++ Assets/TestCarController.cs
@@ -28,92 +28,120 @@ public class TestCarController : MonoBehaviour
     
     public SpawnController spawnController;
 
+    public float MoveSpeed = 50f;
+    public float TurnSpeed = 0.8f;
+
     private void Start()
     {
-        _rb = gameObject.GetComponent<Rigidbody>();
+        // _rb = gameObject.GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
+        float reduceCenterMassY = -0.5f;
+        _rb.centerOfMass = new Vector3(_rb.centerOfMass.x, reduceCenterMassY , _rb.centerOfMass.z);
         CarEngine.Play();
     }
 
     private void Update()
     {
-        _rb.velocity += transform.forward * 150 * Time.deltaTime;
-
-        Wheel1.Rotate(0, 0, -500 * Time.deltaTime);
-        Wheel2.Rotate(0, 0, -500 * Time.deltaTime);
-        Wheel3.Rotate(0, 0, 500 * Time.deltaTime);
-        Wheel4.Rotate(0, 0, 500 * Time.deltaTime);
-        
-        // if (Input.GetKey("w"))
+        // // _rb.velocity += transform.forward * 150 * Time.deltaTime;
+        //
+        // Wheel1.Rotate(0, 0, -500 * Time.deltaTime);
+        // Wheel2.Rotate(0, 0, -500 * Time.deltaTime);
+        // Wheel3.Rotate(0, 0, 500 * Time.deltaTime);
+        // Wheel4.Rotate(0, 0, 500 * Time.deltaTime);
+        //
+        // appliedSpeed += Time.deltaTime * 10f;
+        // appliedSpeed = Mathf.Min (appliedSpeed, speed);
+        // _rb.AddRelativeForce(0,0,appliedSpeed);
+        //
+        // // if (Input.GetKey("w"))
+        // // {
+        // //     RB.velocity += transform.forward * 150 * Time.deltaTime;
+        // //
+        // //     Wheel1.Rotate(0, 0, -500 * Time.deltaTime);
+        // //     Wheel2.Rotate(0, 0, -500 * Time.deltaTime);
+        // //     Wheel3.Rotate(0, 0, 500 * Time.deltaTime);
+        // //     Wheel4.Rotate(0, 0, 500 * Time.deltaTime);
+        // //
+        // //     if(CarEngine.isPlaying == false)
+        // //     {
+        // //         CarEngine.Play();
+        // //     }
+        // // }
+        // // else
+        // // {
+        // //     CarEngine.Stop();
+        // // }
+        //
+        // // if (Input.GetKey("s"))
+        // // {
+        // //     RB.velocity -= transform.forward * 80 * Time.deltaTime;
+        // //
+        // //     Wheel1.Rotate(0, 0, 500 * Time.deltaTime);
+        // //     Wheel2.Rotate(0, 0, 500 * Time.deltaTime);
+        // //     Wheel3.Rotate(0, 0, -500 * Time.deltaTime);
+        // //     Wheel4.Rotate(0, 0, -500 * Time.deltaTime);
+        // // }
+        //
+        // if(Input.GetKey("a"))
         // {
-        //     RB.velocity += transform.forward * 150 * Time.deltaTime;
+        //     // transform.Rotate(0, -30 * Time.deltaTime, 0);
+        //     // CarBody.transform.rotation = Quaternion.Lerp(CarBody.transform.rotation, Left.rotation, 4 * Time.deltaTime);
+        //     // _rb.velocity += CarBody.transform.forward * 120 * Time.deltaTime;
+        //     // _rb.velocity -= transform.forward * 110 * Time.deltaTime;
+        //     _rb.rotation = _rb.rotation * Quaternion.Euler (Vector3.up * 5f);
         //
-        //     Wheel1.Rotate(0, 0, -500 * Time.deltaTime);
-        //     Wheel2.Rotate(0, 0, -500 * Time.deltaTime);
-        //     Wheel3.Rotate(0, 0, 500 * Time.deltaTime);
-        //     Wheel4.Rotate(0, 0, 500 * Time.deltaTime);
+        // }
         //
-        //     if(CarEngine.isPlaying == false)
+        // if(Input.GetKey("d"))
+        // {
+        //     transform.Rotate(0, 30 * Time.deltaTime, 0);
+        //     CarBody.transform.rotation = Quaternion.Lerp(CarBody.transform.rotation, Right.rotation, 4 * Time.deltaTime);
+        //     _rb.velocity += CarBody.transform.forward * 120 * Time.deltaTime;
+        //     _rb.velocity -= transform.forward * 110 * Time.deltaTime;
+        // }
+        //
+        // if(!Input.GetKey("d") && !Input.GetKey("a"))
+        // {
+        //     CarBody.transform.rotation = Quaternion.Lerp(CarBody.transform.rotation, Straight.rotation, 4 * Time.deltaTime);
+        // }
+        //
+        // if(CarBody.transform.localRotation.y * 100 > 15 || CarBody.transform.localRotation.y * 100 < -15)
+        // {
+        //     Trail1.emitting = true;
+        //     Trail2.emitting = true;
+        //     Trail3.emitting = true;
+        //     Trail4.emitting = true;
+        //
+        //     if(DriftCheck == false)
         //     {
-        //         CarEngine.Play();
+        //         DriftCheck = true;
+        //         CarDrift.Play();
         //     }
         // }
         // else
         // {
-        //     CarEngine.Stop();
-        // }
-
-        // if (Input.GetKey("s"))
-        // {
-        //     RB.velocity -= transform.forward * 80 * Time.deltaTime;
+        //     DriftCheck = false;
         //
-        //     Wheel1.Rotate(0, 0, 500 * Time.deltaTime);
-        //     Wheel2.Rotate(0, 0, 500 * Time.deltaTime);
-        //     Wheel3.Rotate(0, 0, -500 * Time.deltaTime);
-        //     Wheel4.Rotate(0, 0, -500 * Time.deltaTime);
+        //     Trail1.emitting = false;
+        //     Trail2.emitting = false;
+        //     Trail3.emitting = false;
+        //     Trail4.emitting = false;
         // }
+    }
 
-        if(Input.GetKey("a"))
+    void FixedUpdate()
+    {
+        float vert = Input.GetAxis("Vertical");
+        float hor = Input.GetAxis("Horizontal");
+        
+        if (vert > 0)
         {
-            transform.Rotate(0, -30 * Time.deltaTime, 0);
-            CarBody.transform.rotation = Quaternion.Lerp(CarBody.transform.rotation, Left.rotation, 4 * Time.deltaTime);
-            _rb.velocity += CarBody.transform.forward * 120 * Time.deltaTime;
-            _rb.velocity -= transform.forward * 110 * Time.deltaTime;
+            _rb.AddRelativeForce(Vector3.forward * vert * MoveSpeed, ForceMode.Acceleration);
         }
 
-        if(Input.GetKey("d"))
+        if (hor != 0)
         {
-            transform.Rotate(0, 30 * Time.deltaTime, 0);
-            CarBody.transform.rotation = Quaternion.Lerp(CarBody.transform.rotation, Right.rotation, 4 * Time.deltaTime);
-            _rb.velocity += CarBody.transform.forward * 120 * Time.deltaTime;
-            _rb.velocity -= transform.forward * 110 * Time.deltaTime;
-        }
-
-        if(!Input.GetKey("d") && !Input.GetKey("a"))
-        {
-            CarBody.transform.rotation = Quaternion.Lerp(CarBody.transform.rotation, Straight.rotation, 4 * Time.deltaTime);
-        }
-
-        if(CarBody.transform.localRotation.y * 100 > 15 || CarBody.transform.localRotation.y * 100 < -15)
-        {
-            Trail1.emitting = true;
-            Trail2.emitting = true;
-            Trail3.emitting = true;
-            Trail4.emitting = true;
-
-            if(DriftCheck == false)
-            {
-                DriftCheck = true;
-                CarDrift.Play();
-            }
-        }
-        else
-        {
-            DriftCheck = false;
-
-            Trail1.emitting = false;
-            Trail2.emitting = false;
-            Trail3.emitting = false;
-            Trail4.emitting = false;
+            _rb.AddRelativeTorque(Vector3.up * hor * TurnSpeed, ForceMode.VelocityChange);
         }
     }
 
